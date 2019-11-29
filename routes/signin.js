40ee9fc844a86469ebc0652945e2b4a22771a674
Sammie <sammieg@umich.edu>
2019-11-28 18:49:50 -0800

    Async auth

--- routes/signin.js
+++ routes/signin.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+let map = new Map([["key1, value1"], ['key2, value2']])
+
 router.get('/', function(req, res, next) {
     console.log('show the sign in page')
 
@@ -12,8 +14,26 @@ router.post('/', function(req, res) {
     const username = req.body.username
     const password = req.body.password
 
+    authenticate(username, password)
+        .then((authenticationStatus) => {
+            if (authenticationStatus === true) {
+                console.log("authenticated")
+            } else {
+                console.log("authentication failed")
+            }
+        }).catch((error) => {
+            console.log('Error: ' + error)
+    })
+
     // check username's password with the provided. Just redirect to sign in page since we're not validating that logic
     res.redirect('/signin')
 });
+
+async function authenticate(username, password) {
+    return new Promise((resolve, reject) => {
+        const actual = map.get(username)
+        return new Promise(actual === password)
+    })
+}
 
 module.exports = router
